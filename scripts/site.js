d5d3aca29d3e2c230b75a65d588b0ab9ded60722
Ibrahim <ibrahimbahraj@gmail.com>
2022-10-22 00:14:54 -0400

    Rebuiled navbar, structure and mortgage calc

---
+++ scripts/site.js
@@ -0,0 +1,57 @@
+"use scripts";
+
+// scripts for mortgage calculator
+var ltpid;
+var ipyid;
+var mortgageid;
+var mrtgname;
+
+window.onload = function()
+{
+    document.getElementById("ipyid").focus();
+  document.getElementById("sbt").onclick = getValues;
+
+
+};
+
+// using "toFixed" to set the precision of the value
+function getValues(){
+    ltpid = document.getElementById("ltpid").value;
+    ipyid = document.getElementById("ipyid").value;
+    mortgageid = document.getElementById("mortgageid").value;
+
+    ipyid /= 1200;
+    ltpid *= 12;
+    rcname = calculatePayment();
+    document.getElementById("rcvalue").value = "$" +  rcname.toFixed(2);
+};
+
+
+function calculatePayment()
+{
+	var payment = mortgageid*(ipyid * Math.pow((1 + ipyid), ltpid))/(Math.pow((1 + ipyid), ltpid) - 1);
+	return payment;
+}
+
+
+
+// scripts for converting from fahrenheit to celcius
+
+var fahid;
+var celsid;
+var rcvale2;
+var fahname;
+
+window.onload =function()
+{
+  document.getElementById("fahid");
+  document.getElementById("sbtn").onclick = getValues;
+};
+
+function getValues()
+{
+  fahid=getElementById("fahid").value;
+  celsid=getElementById("celsid").value;
+
+}
+
